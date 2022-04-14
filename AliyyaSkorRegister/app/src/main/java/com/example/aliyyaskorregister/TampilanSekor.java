041b5270544064a87fe7f6e11130b620f16394fb
aliyyaKh <aliyyalelong27@gmail.com>
2022-04-14 13:19:35 +0700

    Initial commit

---
+++ AliyyaSkorRegister/app/src/main/java/com/example/aliyyaskorregister/TampilanSekor.java
@@ -0,0 +1,14 @@
+package com.example.aliyyaskorregister;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TampilanSekor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tampilan_sekor);
+    }
+}
